7b4b9daca98686c890ff53516d4c8a93d44f2e6a
dabibu153 <68466028+dabibu153@users.noreply.github.com>
2020-08-02 20:53:33 +0530

    added mongoose

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const genre = require("./routes/genre");
+const customer = require("./routes/customer");
+
+mongoose
+  .connect("mongodb://localhost/video_app")
+  .then(() => console.log("connected to server...."))
+  .catch((err) => console.log(err.message));
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("HomePage");
-});
-
 app.use("/api/genre", genre);
+app.use("/api/customer", customer);
 app.listen((PORT = 3000));
--- routes/customer.js
+++ routes/customer.js
@@ -3,34 +3,41 @@ const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
 
-const genreSchema = new mongoose.Schema({
+const customerSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
+  isGold: Boolean,
+  phone: {
+    type: Number,
+    required: true,
+  },
 });
 
-const Genre = new mongoose.model("Genre", genreSchema);
+const Customer = new mongoose.model("Customer", customerSchema);
 
 router.get("/", async (req, res) => {
-  const genres = await Genre.find();
-  res.send(genres);
+  const customers = await Customer.find();
+  res.send(customers);
 });
 
 router.get("/:id", async (req, res) => {
-  const genre = await Genre.findById(req.params.id);
+  const customer = await Customer.findById(req.params.id);
 
-  if (!genre) {
+  if (!customer) {
     res.status(404).send("resourse not found");
     return;
   }
 
-  res.send(genre);
+  res.send(customer);
 });
 
 router.post("/", async (req, res) => {
   const schema = Joi.object({
     name: Joi.string().required(),
+    isGold: Joi.bool(),
+    phone: Joi.number(),
   });
 
   const result = schema.validate(req.body);
@@ -38,16 +45,20 @@ router.post("/", async (req, res) => {
     res.status(400).send("bad input");
     return;
   }
-  let new_genre = new Genre({
+  let new_customer = new Customer({
     name: req.body.name,
+    isGold: req.body.isGold,
+    phone: req.body.phone,
   });
-  await new_genre.save();
-  res.send(new_genre);
+  await new_customer.save();
+  res.send(new_customer);
 });
 
 router.put("/:id", async (req, res) => {
   const schema = Joi.object({
     name: Joi.string().required(),
+    isGold: req.body.isGold,
+    phone: req.body.phone,
   });
 
   const result = schema.validate(req.body);
@@ -55,27 +66,27 @@ router.put("/:id", async (req, res) => {
     res.status(400).send("bad input");
     return;
   }
-  const genre = await Genre.findByIdAndUpdate(
+  const customer = await Customer.findByIdAndUpdate(
     req.params.id,
-    { name: req.body.name },
+    { name: req.body.name, isGold: req.body.isGold, phone: req.body.phone },
     { new: true }
   );
 
-  if (!genre) {
+  if (!customer) {
     res.status(404).send("resourse not found");
     return;
   }
-  res.send(genre);
+  res.send(customer);
 });
 
 router.delete("/:id", async (req, res) => {
-  const genre = await Genre.findByIdAndRemove(req.params.id);
+  const customer = await Customer.findByIdAndRemove(req.params.id);
 
-  if (!genre) {
+  if (!customer) {
     res.status(404).send("resourse not found");
     return;
   }
-  res.send(genre);
+  res.send(customer);
 });
 
 module.exports = router;
